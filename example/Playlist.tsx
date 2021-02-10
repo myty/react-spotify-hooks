adbdd25612bdf1bf67eece891c853356f9d8df66
Michael Tyson <michaeltyson@outlook.com>
2021-02-10 07:52:17 -0500

    make a more polished example

---
+++ example/Playlist.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { SpotifyPlaylist } from '../src/models/Spotify/playlist';
+
+interface PlaylistProps {
+    playlist: SpotifyPlaylist;
+}
+
+export function Playlist({ playlist }: PlaylistProps) {
+    const imageUrl = playlist.images.length > 0 ? playlist.images[0].url : '';
+
+    return (
+        <li
+            className="flex overflow-hidden mx-4 my-2 text-left bg-gray-100 rounded border border-gray-400"
+            key={playlist.id}
+        >
+            <div className="flex-none w-32 h-32">
+                <img
+                    className="object-cover w-full h-full rounded-l"
+                    src={imageUrl}
+                    alt={`${playlist.name} cover`}
+                />
+            </div>
+            <div className="flex-grow m-2">
+                <h2 className="h-6 overflow-hidden font-semibold">
+                    {playlist.name}
+                </h2>
+                <h3 className="text-sm">{playlist.tracks?.total} songs</h3>
+                <span className="block text-xs">
+                    by {playlist.owner?.display_name}
+                </span>
+            </div>
+        </li>
+    );
+}
--- example/my-playlists.tsx
+++ example/my-playlists.tsx
@@ -2,27 +2,40 @@ import * as React from 'react';
 import { useEffect, useState } from 'react';
 import { useSpotify } from '../src/hooks';
 import { SpotifyPlaylist } from '../src/models/Spotify/playlist';
+import { Playlist } from './Playlist';
 
 export default function MyPlaylists() {
     const { getCurrentUserPlaylists } = useSpotify();
 
     const [playlists, setPlaylists] = useState<SpotifyPlaylist[]>([]);
+    const [nextPlaylistUrl, setNextPlaylistUrl] = useState<string>();
+    const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
-        async function getPlaylists() {
-            const playlistsResult = await getCurrentUserPlaylists();
-            setPlaylists(prev => [...prev, ...(playlistsResult?.items ?? [])]);
+        if (loaded) {
+            return;
         }
 
-        getPlaylists();
-    }, []);
+        (async function getPlaylists() {
+            const playlistsResult = await getCurrentUserPlaylists(
+                nextPlaylistUrl
+            );
+
+            if (playlistsResult?.next == null) {
+                setLoaded(true);
+            }
+
+            setPlaylists(prev => [...prev, ...(playlistsResult?.items ?? [])]);
+            setNextPlaylistUrl(playlistsResult?.next ?? undefined);
+        })();
+    }, [nextPlaylistUrl]);
 
     return (
         <>
-            <h1>My Playlists</h1>
+            <h1 className="p-4 text-xl font-bold">My Playlists</h1>
             <ul>
                 {playlists.map(playlist => (
-                    <li>{playlist.name}</li>
+                    <Playlist playlist={playlist} />
                 ))}
             </ul>
         </>
