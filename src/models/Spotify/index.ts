7a55cc5b256348949ecef9043e562c8b460f4300
Michael Tyson <myty@users.noreply.github.com>
2021-02-14 17:27:18 -0500

    version 0.1.0 changes (#2)
    * use-spotify, use-playlist, example not working
    
    * update context provider
    
    * improve error handling
    
    * make a more polished example
    
    * update npm packages
    
    * update exports
    
    * cleanup

---
+++ src/models/Spotify/index.ts
@@ -0,0 +1,17 @@
+export { SpotifyAlbum } from './album';
+export { SpotifyArtist } from './artist';
+export {
+    SpotifyPaging,
+    SpotifyUser,
+    SpotifyExternalUrls,
+    SpotifyExternalIds,
+    SpotifyImage,
+    SpotifyProductType,
+    SpotifyReference,
+} from './core';
+export { SpotifyPlaylist } from './playlist';
+export {
+    SpotifyTrackWithMetadata,
+    SpotifyTrackLink,
+    SpotifyTrack,
+} from './track';
