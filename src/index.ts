059fde2bab2a34230b1f902c7ae11367519e7778
Michael Tyson <michaeltyson@outlook.com>
2021-02-14 17:16:14 -0500

    update exports

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// Hooks
+export * from './hooks';
+
+// Context Provider
+export { SpotifyContextProvider } from './context/spotify-context-provider';
+
+// Types
+export * from './models/Spotify';
